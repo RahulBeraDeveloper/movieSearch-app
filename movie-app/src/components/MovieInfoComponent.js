835689619449565ee4ef17c2640498ed6af2e581
Rahul Bera <rahaulbera3654@gmail.com>
2023-09-19 16:19:32 +0530

    first commit

---
+++ movie-app/src/components/MovieInfoComponent.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { API_KEY } from "../App";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+`;
+
+const CoverImage = styled.img`
+  object-fit: cover;
+  width: 100%;
+  max-height: 350px;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    width: auto;
+    max-height: none;
+    margin-right: 20px;
+    margin-bottom: 0;
+  }
+`;
+
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+
+  @media (min-width: 768px) {
+    flex: 1;
+  }
+`;
+
+const MovieName = styled.span`
+  font-size: 22px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+
+  & span {
+    opacity: 0.8;
+  }
+`;
+
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  overflow: hidden;
+  margin: 4px 0;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+
+  & span {
+    opacity: 0.5;
+  }
+`;
+
+const Close = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  color: black;
+  background: lightgray;
+  height: fit-content;
+  padding: 8px;
+  border-radius: 50%;
+  cursor: pointer;
+  opacity: 0.8;
+  align-self: flex-end;
+
+  @media (min-width: 768px) {
+    align-self: flex-start;
+  }
+`;
+
+const MovieInfoComponent = (props) => {
+  const [movieInfo, setMovieInfo] = useState();
+  const { selectedMovie } = props;
+
+  useEffect(() => {
+    Axios.get(
+      `https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`
+    ).then((response) => setMovieInfo(response.data));
+  }, [selectedMovie]);
+
+  return (
+    <Container>
+      {movieInfo ? (
+        <>
+          <CoverImage src={movieInfo?.Poster} alt={movieInfo?.Title} />
+          <InfoColumn>
+            <MovieName>
+              {movieInfo?.Type}: <span>{movieInfo?.Title}</span>
+            </MovieName>
+            <MovieInfo>
+              IMDB Rating: <span>{movieInfo?.imdbRating}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Year: <span>{movieInfo?.Year}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Language: <span>{movieInfo?.Language}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Rated: <span>{movieInfo?.Rated}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Released: <span>{movieInfo?.Released}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Runtime: <span>{movieInfo?.Runtime}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Genre: <span>{movieInfo?.Genre}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Director: <span>{movieInfo?.Director}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Actors: <span>{movieInfo?.Actors}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Plot: <span>{movieInfo?.Plot}</span>
+            </MovieInfo>
+          </InfoColumn>
+          <Close onClick={() => props.onMovieSelect()}>X</Close>
+        
+        </>
+      ) : (
+        "Loading..."
+      )}
+    </Container>
+  );
+};
+
+export default MovieInfoComponent;
